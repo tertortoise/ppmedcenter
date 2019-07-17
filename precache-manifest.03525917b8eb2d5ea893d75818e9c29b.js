9e02d3cba1faf5f759881ff2b4d5628568bbbbd8
tertortoise <tertortoise@yandex.ru>
2019-07-17 09:27:54 +0300

    Updates

---
+++ precache-manifest.03525917b8eb2d5ea893d75818e9c29b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2913c4565e1ce48b2b096ac81001047",
+    "url": "/ppmedcenter/index.html"
+  },
+  {
+    "revision": "03980f1dfba857070295",
+    "url": "/ppmedcenter/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "71f413a964f72bdd983f",
+    "url": "/ppmedcenter/static/css/main.c48def99.chunk.css"
+  },
+  {
+    "revision": "03980f1dfba857070295",
+    "url": "/ppmedcenter/static/js/2.954ca1bb.chunk.js"
+  },
+  {
+    "revision": "71f413a964f72bdd983f",
+    "url": "/ppmedcenter/static/js/main.86138eb5.chunk.js"
+  },
+  {
+    "revision": "b38fce13f90cea0d0a1c",
+    "url": "/ppmedcenter/static/js/runtime~main.7d34283f.js"
+  },
+  {
+    "revision": "a6b658636d77271aec8de538fc64fb0c",
+    "url": "/ppmedcenter/static/media/docFemale.a6b65863.jpg"
+  },
+  {
+    "revision": "269f45593891430923bd0c80e36e1834",
+    "url": "/ppmedcenter/static/media/docMale.269f4559.jpg"
+  },
+  {
+    "revision": "2f22e7c7479da757bb05a0dbba9a6b2e",
+    "url": "/ppmedcenter/static/media/imageBlank.2f22e7c7.svg"
+  },
+  {
+    "revision": "02421cbadcd246f589d6f3dd3b736692",
+    "url": "/ppmedcenter/static/media/img.02421cba.jpg"
+  }
+]);
